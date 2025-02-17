a2254207eb83768be4bcff876f15daf0311701b6
Daniel Monroy <fernandodms04@gmail.com>
2025-02-17 12:12:02 -0600

    Finish header

--- Web/Exercises/web-app-1/src/components/Header.jsx
+++ Web/Exercises/web-app-1/src/components/Header.jsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
     return (
-        <div className="flex w-screen h-24">
-            <Link href=".">Home</Link>
-            <Link href="Products">Products</Link>
-            <Link href="TimeMachine">TimeMachine</Link>
+        <div className="flex w-auto h-24 shadow-xl">
+            <div className="flex items-center px-20 bg-green-300">
+                <Link href=".">
+                    <Image
+                        src="/images/logo-plant-shop.webp"
+                        alt="Logo"
+                        width={400}
+                        height={100}
+                    />
+                </Link>
+            </div>
+            <div className="flex w-screen items-center justify-evenly text-2xl font-bold bg-green-400">
+                <Link href=".">Home</Link>
+                <Link href="Products">Products</Link>
+                <Link href="TimeMachine">TimeMachine</Link>
+            </div>
         </div>
     );
 }
