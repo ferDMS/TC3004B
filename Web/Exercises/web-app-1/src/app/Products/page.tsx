d6ce64f8cb01ff4b2aa8060c6686e5da9a018fc3
Daniel Monroy <fernandodms04@gmail.com>
2025-02-17 11:40:37 -0600

    Add navigation

--- Web/Exercises/web-app-1/src/app/Products/page.tsx
+++ Web/Exercises/web-app-1/src/app/Products/page.tsx
@@ -1,10 +1,11 @@
-import Header from "../components/Header";
+import Link from "next/link";
+import Header from "../../components/Header";
 
 export default function Home() {
     return (
         <div className="flex flex-col">
             <Header></Header>
-            <div>Home!</div>
+            <div>Products!</div>
         </div>
     );
 }
