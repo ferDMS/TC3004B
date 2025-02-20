559c24e2355b055906106b1cd3b77cac5c9ef1c1
Daniel Monroy <fernandodms04@gmail.com>
2025-02-20 11:27:01 -0600

    Header Layout and Home

--- Web/Exercises/web-app-1/src/app/Products/page.tsx
+++ Web/Exercises/web-app-1/src/app/Products/page.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Header from "../../components/Header";
 
 export default function Home() {
     return (
         <div className="flex flex-col">
-            <Header></Header>
             <div>Products!</div>
         </div>
     );
--- Web/Exercises/web-app-1/src/app/page.tsx
+++ Web/Exercises/web-app-1/src/app/page.tsx
@@ -1,10 +1,10 @@
-import Header from "../components/Header";
-
 export default function Home() {
     return (
-        <div className="flex flex-col">
-            <Header></Header>
-            <div>Home!</div>
+        <div className="flex flex-col h-dvh">
+            <div className="flex flex-col h-full items-center justify-center">
+                <div className="text-8xl font-bold">Plant Shop</div>
+                <div className="text-xl">Holas</div>
+            </div>
         </div>
     );
 }
